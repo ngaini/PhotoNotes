4667c24969cca7b4a0a2e8fc5a55fae9c6061db5
Nathan Gaini <nathan.gaini3@gmail.com>
2016-02-13 16:27:31 -0800

    camera fab launches camera

--- app/src/main/java/com/ngaini/photonotes/AddPhotoActivity.java
+++ app/src/main/java/com/ngaini/photonotes/AddPhotoActivity.java
@@ -1,9 +1,12 @@
 package com.ngaini.photonotes;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class AddPhotoActivity extends ActionBarActivity {
@@ -12,6 +15,18 @@ public class AddPhotoActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_photo);
+
+        //action for the FAB add photo button
+        FloatingActionButton fab_camera = (FloatingActionButton) findViewById(R.id.fab_addPhoto_button);
+
+        fab_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(camera_intent, 0);
+            }
+        });
+
     }
 
 
